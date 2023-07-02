3e5cb380317465080b4d7bc79808a43357499f77
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2023-07-02 18:10:55 +0300

    Integrated validation using the ValidationPipe

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,15 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  IsOptional,
+  MaxLength,
+  IsBoolean,
+} from 'class-validator';
 
 export class CreateArticleDto {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
+  @MinLength(5)
   title: string;
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
+  @MaxLength(300)
   @ApiProperty({ required: false })
   description?: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   body: string;
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   published?: boolean = false;
 }
