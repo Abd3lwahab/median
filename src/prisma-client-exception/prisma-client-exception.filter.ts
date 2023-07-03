5702313388c100e19c1bdaf3e6a90adde4c4ccba
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2023-07-03 13:05:48 +0300

    handle Prisma errors with exception filter

---
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -0,0 +1,38 @@
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    console.error(exception.message);
+
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    switch (exception.code) {
+      case 'P2002': {
+        const status = HttpStatus.CONFLICT;
+        response.status(status).json({
+          statusCode: status,
+          message:
+            'Unique constraint failed on the database. Please check your input and try again.',
+        });
+        break;
+      }
+      case 'P2025': {
+        const status = HttpStatus.NOT_FOUND;
+        response.status(status).json({
+          statusCode: status,
+          message: 'Record not found.',
+        });
+        break;
+      }
+      default:
+        // default 500 error code
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
