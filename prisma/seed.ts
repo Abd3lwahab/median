9f0405e21608a11787c30fcae8e5b5688bd1c3a8
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2023-07-03 19:39:38 +0300

    Add user model to schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,31 +3,59 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const article1 = await prisma.article.upsert({
-    where: { title: 'Prisma With Nest.js and Postgresql' },
+  // create two dummy users
+  const user1 = await prisma.user.upsert({
+    where: { email: 'sabin@adams.com' },
     update: {},
     create: {
-      title: 'Prisma With Nest.js and Postgresql',
+      email: 'sabin@adams.com',
+      name: 'Sabin Adams',
+      password: 'password-sabin',
+    },
+  });
+
+  const user2 = await prisma.user.upsert({
+    where: { email: 'alex@ruheni.com' },
+    update: {},
+    create: {
+      email: 'alex@ruheni.com',
+      name: 'Alex Ruheni',
+      password: 'password-alex',
+    },
+  });
+
+  // create three dummy articles
+  const post1 = await prisma.article.upsert({
+    where: { title: 'Prisma Adds Support for MongoDB' },
+    update: {
+      authorId: user1.id,
+    },
+    create: {
+      title: 'Prisma Adds Support for MongoDB',
       body: 'Support for MongoDB has been one of the most requested features since the initial release of...',
       description:
         "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
       published: false,
+      authorId: user1.id,
     },
   });
 
-  const article2 = await prisma.article.upsert({
+  const post2 = await prisma.article.upsert({
     where: { title: "What's new in Prisma? (Q1/22)" },
-    update: {},
+    update: {
+      authorId: user2.id,
+    },
     create: {
       title: "What's new in Prisma? (Q1/22)",
       body: 'Our engineers have been working hard, issuing new releases with many improvements...',
       description:
         'Learn about everything in the Prisma ecosystem and community from January to March 2022.',
       published: true,
+      authorId: user2.id,
     },
   });
 
-  console.log({ article1, article2 });
+  console.log({ user1, user2, post1, post2 });
 }
 
 main()
